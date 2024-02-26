7027292efb7633783d9b7b91629f62a8cc90cdae
egorov-ma <link@egorovma.ru>
2024-02-27 02:07:47 +0400

    upd testBase

--- src/test/java/ru/chitaigorod/api/tests/TestBase.java
+++ src/test/java/ru/chitaigorod/api/tests/TestBase.java
@@ -1,12 +1,20 @@
 package ru.chitaigorod.api.tests;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     @BeforeAll
     public static void beforeAll() {
         RestAssured.baseURI = "https://web-gate.chitai-gorod.ru";
-        RestAssured.basePath = "/api/";
+        RestAssured.basePath = "/api";
+    }
+
+    @AfterEach
+    void tearDown() {
+        closeWebDriver();
     }
 }
