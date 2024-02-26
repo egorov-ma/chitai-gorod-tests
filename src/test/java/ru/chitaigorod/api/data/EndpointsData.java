ce9dfa16eb863ea7b86b8d55bff82ee52597d921
egorov-ma <link@egorovma.ru>
2024-02-27 02:08:12 +0400

    upd data

--- src/test/java/ru/chitaigorod/api/data/EndpointsData.java
+++ src/test/java/ru/chitaigorod/api/data/EndpointsData.java
@@ -1,8 +1,9 @@
 package ru.chitaigorod.api.data;
 
 public class EndpointsData {
-    public final static String CART = "v1/cart",
-            CART_SHORT = "v1/cart/short",
-            PRODUCT = "v1/cart/product",
-            PRODURT_SEARCH = "v2/search/product?phrase=";
+    public final static String CART = "/v1/cart",
+            CART_SHORT = "/v1/cart/short",
+            PRODUCT = "/v1/cart/product",
+            PRODUCT_SEARCH = "/v2/search/product",
+            SEARCH_PARAM = "phrase";
 }
--- src/test/java/ru/chitaigorod/api/data/ProductsData.java
+++ src/test/java/ru/chitaigorod/api/data/ProductsData.java
@@ -1,8 +1,14 @@
 package ru.chitaigorod.api.data;
 
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
 public class ProductsData {
 
-    public final String productItem = "чистый код";
+    private final Faker faker = new Faker(new Locale("ru"));
+    public final String productItem = "чистый код",
+    randomProductItem = faker.book().author().toLowerCase();
     public final int goodsId = 2640391,
             errorId = 1,
             productId = 112163558;
