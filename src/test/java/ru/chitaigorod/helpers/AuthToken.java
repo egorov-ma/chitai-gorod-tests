7fb49586e5ca2846be36674963b6669871ca3105
egorov-ma <link@egorovma.ru>
2024-02-27 22:13:48 +0400

    fix

--- src/test/java/ru/chitaigorod/helpers/AuthToken.java
+++ src/test/java/ru/chitaigorod/helpers/AuthToken.java
@@ -14,6 +14,9 @@ public class AuthToken {
     public static String getAccessToken() {
         // todo лютый костыль но локально работает а в jenkins не возвращается токен. надо будет поправть if()
         Map<String, String> allCookies = get("https://www.chitai-gorod.ru/").getCookies();
+        System.out.println("########");
+        allCookies.forEach((key, value) -> System.out.println(key + ":" + value));
+        System.out.println("########");
         String accessToken = allCookies.get("access-token");
         if (!accessToken.isEmpty()) {
             accessToken = accessToken.replace("%20", " ");
