f9bdfc364d6b8723bb8e5350f9b34b0fb42c7769
egorov-ma <link@egorovma.ru>
2024-02-27 22:11:51 +0400

    fix

--- src/test/java/ru/chitaigorod/helpers/AuthToken.java
+++ src/test/java/ru/chitaigorod/helpers/AuthToken.java
@@ -5,14 +5,21 @@ import java.util.Map;
 import static io.restassured.RestAssured.get;
 
 public class AuthToken {
+    public final static String TEMP_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" +
+            ".eyJleHAiOjE3MDkyMjUwMTQsImlhdCI6MTcwOTA1NzAxNCwiaXNzIjoiL2FwaS92MS9hdX" +
+            "RoL2Fub255bW91cyIsInN1YiI6ImI1ZDM3ZDM1ZTg5NjQyYzIxOGYyZDYwYjU4NjU2YmI4M" +
+            "2JiMjZhZGRlOGRmYjRiODRlNTEyOTA0YjZkOTRkMGEiLCJ0eXBlIjoxMH0" +
+            ".vhVqgbYfleN9fOYXsgwQETcy09I6r0laSijT6rSyLCA";
+
     public static String getAccessToken() {
+        // todo лютый костыль но локально работает а в jenkins не возвращается токен. надо будет поправть if()
         Map<String, String> allCookies = get("https://www.chitai-gorod.ru/").getCookies();
-        System.out.println("###########################");
-        System.out.println(allCookies);
-        System.out.println("###########################");
-        allCookies.forEach((key, value) -> System.out.println(key + ":" + value));
         String accessToken = allCookies.get("access-token");
-        accessToken = accessToken.replace("%20", " ");
-        return accessToken;
+        if (!accessToken.isEmpty()) {
+            accessToken = accessToken.replace("%20", " ");
+            return accessToken;
+        } else {
+            return TEMP_TOKEN;
+        }
     }
 }
